b0ec85d7ef04e9352c813641022da5f9139394f4
RajMujmer <rajat.mujmer0206@gmail.com>
2024-10-31 06:53:50 +0530

    first.py

--- first.py
+++ first.py
@@ -6,6 +6,14 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+st.markdown("""
+<style>
+body {
+    background-color: #YOUR_COLOR;
+}
+</style>
+""", unsafe_allow_html=True)
+
 # Show app title and description.
 st.set_page_config(page_title="Speaker", page_icon="🎫")
 st.title(" My Speaker ")
