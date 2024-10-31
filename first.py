e2bcd329ef3a1d37dc25b8b2d1a0a9fa8aa1d2b9
RajMujmer <rajat.mujmer0206@gmail.com>
2024-10-31 07:11:41 +0530

    first.py

--- first.py
+++ first.py
@@ -1,17 +1,18 @@
 import datetime
 import random
-
 import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+
+# Page configuration must be the first command
 st.set_page_config(
     page_title="Custom Background",
     page_icon=":art:",
     layout="centered"
 )
 
-
+# CSS to set background color
 page_bg = """
 <style>
 .stApp {
@@ -23,21 +24,15 @@ page_bg = """
 # Apply the background color using markdown
 st.markdown(page_bg, unsafe_allow_html=True)
 
-
-# Show app title and description.
-st.set_page_config(page_title="Speaker", page_icon="🎫")
+# Show app title and description
 st.title(" My Speaker ")
-st.header(
-    """
-   Table 
-    """
-)
+st.header("Table")
+
 with st.form("Table of N"):
     issue = st.text_area("Number of which you want a table ")
-    #priority = st.selectbox("Priority", ["High", "Medium", "Low"])
     submitted = st.form_submit_button("Submit")
 
 if submitted:
-        for j in range(1,11):
-            st.write(issue ," * " , j ,"= ", int(issue)*j )
-        st.write("---------------------------")
+    for j in range(1, 11):
+        st.write(issue, " * ", j, "= ", int(issue) * j)
+    st.write("---------------------------")
